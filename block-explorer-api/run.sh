68251de56a2fc2cb69b2e2eacdf2673aedaebc2c
Michael Vines <mvines@gmail.com>
2019-02-15 20:41:01 -0800

    Remove unused NODE_INFO

--- block-explorer-api/run.sh
+++ block-explorer-api/run.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
-# populate (strawman) node info
-NODE_INFO="{\"host\":\""$(hostname)"\",\"info\":"$(curl -q -s ifconfig.co/json)"}"
-export NODE_INFO
-
-set -x
+set -ex
 redis-cli ping
 yarn run babel-node --presets env index.js
