c62bafe0c703c058c8f0389cfcfae7771570c046
Michael Vines <mvines@gmail.com>
2019-02-15 20:04:17 -0800

    Fail better if redis isn't running

--- block-explorer-api/run.sh
+++ block-explorer-api/run.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 # populate (strawman) node info
 NODE_INFO="{\"host\":\""$(hostname)"\",\"info\":"$(curl -q -s ifconfig.co/json)"}"
 export NODE_INFO
 
+set -x
+redis-cli ping
 yarn run babel-node --presets env index.js
--- block-explorer-inbound-stream/run.sh
+++ block-explorer-inbound-stream/run.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 # populate (strawman) node info
 NODE_INFO="{\"host\":\""$(hostname)"\",\"info\":"$(curl -q -s ifconfig.co/json)"}"
 export NODE_INFO
 
+set -x
+redis-cli ping
 rm -f /tmp/streamtap.sock
 yarn run babel-node --presets env index.js
