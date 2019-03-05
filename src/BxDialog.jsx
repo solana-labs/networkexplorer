97a8c7cf28db7b28fe13ef81b7610f640f65387c
Sunny Gleason <sunny.gleason@gmail.com>
2019-03-04 23:38:40 -0500

    feat: improve dialogs, add txns-by-prgid dialog, remove tick-height #16 #20

--- src/BxDialog.jsx
+++ src/BxDialog.jsx
@@ -1,106 +1,156 @@
-import React from "react";
-import BxDateTime from "./BxDateTime";
+import React from 'react';
+import BxDateTime from './BxDateTime';
 import CloseIcon from '@material-ui/icons/Close';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import QRCode from 'qrcode.react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import BxEntityLink from './BxEntityLink';
 
 const location = window.location.href;
 
 class BxDialog extends React.Component {
-    handleClose = () => {
-        this.props.onClose(this.props.selectedValue);
-    };
+  render() {
+    const {classes, selectedValue, onClose, ...other} = this.props;
 
-    render() {
-        const {classes, onClose, selectedValue, ...other} = this.props;
+    let value = selectedValue || {};
+    let title = null;
+    let url = location;
+    let rows = [];
 
-        let value = selectedValue || {};
-        let title = null;
-        let url = location;
-        let rows = [];
+    rows.push(['Slot #', value.s]);
 
-        if (value.t !== "blk") {
-            rows.push(["Block ID", value.block_id || (value.block && value.block.id) || "<pending>"]);
-        }
-
-        rows.push(["Slot #", value.s]);
-        rows.push(["Tick #", value.h]);
-
-        rows.push(["Timestamp (approx)", BxDateTime.formatDateTime(value.dt), value.dt]);
-
-        if (value.t === "txn") {
-            title = "Transaction";
-            url = url + "txn/" + value.id;
-
-            // TODO: transaction/instruction attributes
-            // rows.push(["Signature(s)", value.instructions[0].signatures.join(", ")]);
-            // rows.push(["Program ID(s)", value.instructions[0].program_id]);
-            // rows.push(["Account(s)", value.instructions[0].keys.join(", ")]);
-        }
-
-        if (value.t === "blk") {
-            title = "Block";
-            url = url + "blk/" + value.id;
-            rows.push(["Entries", value.entries.join(", ")]);
-        }
-
-        if (value.t === "ent") {
-            title = "Entry";
-            url = url + "ent/" + value.id;
-            rows.push(["Transaction(s)", value.transactions.join(", ")]);
-        }
-
-        rows.unshift([title + " ID", value.id]);
-
-        return (
-            <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" fullWidth={true} maxWidth={"lg"} {...other}>
-                <DialogTitle id="simple-dialog-title" title={JSON.stringify(value)}>
-                    {title} Detail
-                    <IconButton aria-label="Close" className={classes.closeButton} onClick={this.handleClose}>
-                        <CloseIcon />
-                    </IconButton>
-                </DialogTitle>
-                <Paper>
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell component="th" scope="row">QR Code:</TableCell>
-                                <TableCell align="right" title={url}>
-                                    <QRCode value={url} style={{ width: "120px", height: "120px" }} />
-                                </TableCell>
-                            </TableRow>
-                            {rows.map(row => (
-                                <TableRow key={JSON.stringify(row)}>
-                                    <TableCell component="th" scope="row">{row[0]}:</TableCell>
-                                    <TableCell align="right" title={row[2]}>{row[1]}</TableCell>
-                                </TableRow>
-                            ))}
-                            <TableRow>
-                                <TableCell component="th" scope="row">Raw Data:</TableCell>
-                                <TableCell align="right" title={url}>
-                                    {JSON.stringify(value, null, 2)}
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </Paper>
-            </Dialog>
-        );
+    if (value.t === 'txn') {
+      title = 'Transaction';
+      url = url + 'txn/' + value.id;
     }
+
+    if (value.t === 'blk') {
+      title = 'Block';
+      url = url + 'blk/' + value.id;
+    }
+
+    if (value.t === 'ent') {
+      title = 'Entry';
+      url = url + 'ent/' + value.id;
+    }
+
+    let block_id = ((value.t === 'blk') && value.id) || (value.block && value.block.id) || '<pending>';
+
+    return (
+      <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" fullWidth={true}
+              maxWidth={'lg'} {...other}>
+        <DialogTitle id="simple-dialog-title" title={JSON.stringify(value)}>
+          {title} Detail
+          <IconButton aria-label="Close" className={classes.closeButton} onClick={onClose}>
+            <CloseIcon/>
+          </IconButton>
+        </DialogTitle>
+        <Paper>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell component="th" scope="row">QR Code:</TableCell>
+                <TableCell align="right" title={url}>
+                  <QRCode value={url} style={{width: '120px', height: '120px'}}/>
+                </TableCell>
+              </TableRow>
+              {value.t === 'txn' && (
+                <TableRow>
+                  <TableCell component="th" scope="row">Transaction ID:</TableCell>
+                  <TableCell align="right" title={url}>
+                    <BxEntityLink txn={value.id}/>
+                  </TableCell>
+                </TableRow>
+              )}
+              {value.t === 'txn' && (
+                <TableRow>
+                  <TableCell component="th" scope="row">Program ID:</TableCell>
+                  <TableCell align="right">
+                    <BxEntityLink prg_id={value.data.instructions[0].program_id}/>
+                  </TableCell>
+                </TableRow>
+              )}
+              {value.t === 'txn' && (
+                <TableRow>
+                  <TableCell component="th" scope="row">Account ID(s):</TableCell>
+                  <TableCell align="right">
+                    {
+                      value.data.instructions[0].keys.map(key => (
+                        <span key={key}>
+                          <BxEntityLink acct_id={key}/>
+                          <span> </span>
+                        </span>
+                      ))
+                    }
+                  </TableCell>
+                </TableRow>
+              )}
+              {/*{value.t !== 'blk' && (*/}
+              {/*<TableRow>*/}
+              {/*<TableCell component="th" scope="row">Entry ID:</TableCell>*/}
+              {/*<TableCell align="right" title={url}>*/}
+              {/*<BxEntityLink ent={entry_id}/>*/}
+              {/*</TableCell>*/}
+              {/*</TableRow>*/}
+              {/*)}*/}
+              <TableRow>
+                <TableCell component="th" scope="row">Block ID:</TableCell>
+                <TableCell align="right" title={url}>
+                  <BxEntityLink blk={block_id}/>
+                </TableCell>
+              </TableRow>
+              {/*{value.t === 'blk' && (*/}
+              {/*<TableRow>*/}
+              {/*<TableCell component="th" scope="row">Entries:</TableCell>*/}
+              {/*<TableCell align="right" title={url}>*/}
+              {/*{*/}
+              {/*value.entries.map(key => (*/}
+              {/*<span key={key}>*/}
+              {/*<BxEntityLink ent={key}/>*/}
+              {/*<span> </span>*/}
+              {/*</span>*/}
+              {/*))*/}
+              {/*}*/}
+              {/*</TableCell>*/}
+              {/*</TableRow>*/}
+              {/*)}*/}
+              {rows.map(row => (
+                <TableRow key={JSON.stringify(row)}>
+                  <TableCell component="th" scope="row">{row[0]}:</TableCell>
+                  <TableCell align="right" title={row[2]}>{row[1]}</TableCell>
+                </TableRow>
+              ))}
+              <TableRow>
+                <TableCell component="th" scope="row">Timestamp (approx):</TableCell>
+                <TableCell align="right">
+                  <BxDateTime dateTime={value.dt} local/>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component="th" scope="row">Raw Data:</TableCell>
+                <TableCell align="right" title={url}>
+                  {JSON.stringify(value, null, 2)}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+      </Dialog>
+    );
+  }
 }
 
 BxDialog.propTypes = {
-    classes: PropTypes.object.isRequired,
-    onClose: PropTypes.func,
-    selectedValue: PropTypes.object,
+  classes: PropTypes.object.isRequired,
+  onClose: PropTypes.func,
+  selectedValue: PropTypes.object,
 };
 
 export default BxDialog;
