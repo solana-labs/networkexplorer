94319d3021a471991a7f2821385523d5dc00af22
Sunny Gleason <sunny.gleason@gmail.com>
2019-03-16 15:51:15 -0400

    feat: gentler treatment of BxHelpLink

--- src/BxHelpLink.jsx
+++ src/BxHelpLink.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import InfoIcon from '@material-ui/icons/Info';
 import Link from '@material-ui/core/Link';
-import {testnetDefaultChannel} from '@solana/web3.js/package.json';
+import { testnetDefaultChannel } from '@solana/web3.js/package.json';
 
 const BOOK_VERSION = (testnetDefaultChannel === 'edge') ? 'book-edge' : 'book';
 
 class BxHelpLink extends React.Component {
   render() {
-    const {text, term} = this.props;
+    const { text, term } = this.props;
     return (
-      <Link
-        href={`https://solana-labs.github.io/${BOOK_VERSION}/terminology.html#${term}`}
-        target="_new"
-      >
-        <i className={'fa fa-question'} title={text} />
-      </Link>
+        <Link
+          href={`https://solana-labs.github.io/${BOOK_VERSION}/terminology.html#${term}`}
+          target="_new"
+        >
+          <InfoIcon fontSize="small" color="disabled" style={{padding:"5px", verticalAlign:'middle'}} />
+        </Link>
     );
   }
 }
