2278bdb08db98f7a7b0fa31d423e1de8f6b64159
Manuel Calavera <diserkin@gmail.com>
2019-07-22 13:05:37 -0700

    feat: mixpanel and ga integration

--- src/v2/components/Footer/Newsletter/index.jsx
+++ src/v2/components/Footer/Newsletter/index.jsx
@@ -1,10 +1,17 @@
 import {InputBase, Typography} from '@material-ui/core';
 import React from 'react';
+import Mixpanel from 'v2/mixpanel';
 
 import useStyles from './styles';
 
 const Newsletter = () => {
   const classes = useStyles();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    Mixpanel.track('Clicked Partner with us');
+  };
+
   return (
     <div>
       <Typography className={classes.title} variant="h5">
@@ -13,7 +20,7 @@ const Newsletter = () => {
       <Typography className={classes.desc}>
         Stay up to date with the latest Solana news.
       </Typography>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <InputBase
           className={classes.input}
           placeholder="Email Address"
