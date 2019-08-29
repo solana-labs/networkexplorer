474abb7b8ed27195e505214246f844446bc75669
Sunny Gleason <sunny.gleason@gmail.com>
2019-08-29 15:09:48 -0400

    fix: remove babel plugin for date-fns

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,5 @@ const {override, addBabelPlugin} = require('customize-cra');
 
 module.exports = override(
   // addBabelPlugin('lodash'),
-  addBabelPlugin('date-fns'),
   addBabelPlugin('react-hot-loader/babel'),
 );
