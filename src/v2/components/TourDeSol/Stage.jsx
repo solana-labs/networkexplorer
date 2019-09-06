ec11bd7178e9aefbf5beb3e6a00b6fd1ac2d9d00
Manuel Calavera <diserkin@gmail.com>
2019-09-06 15:29:07 -0700

    fix: stages are coming soon
    + minor css stats card correction

--- src/v2/components/TourDeSol/Stage.jsx
+++ src/v2/components/TourDeSol/Stage.jsx
@@ -1,11 +1,11 @@
 // @flow
 import React from 'react';
 import cn from 'classnames';
-import format from 'date-fns/format';
-import {eq, lt} from 'lodash/fp';
+// import format from 'date-fns/format';
+import {eq /*lt*/} from 'lodash/fp';
 import {observer} from 'mobx-react-lite';
 import {TDS_ACTIVE_STAGE} from 'v2/constants';
-import iconRight from 'v2/assets/icons/arrow-right-dark.png';
+// import iconRight from 'v2/assets/icons/arrow-right-dark.png';
 
 type StageProps = {
   className: string,
@@ -24,18 +24,21 @@ const Stage = ({className, activeClass, stage}: StageProps) => {
     return null;
   }
   const isActive = eq(stage.id, TDS_ACTIVE_STAGE);
-  const isFinished = lt(stage.id, TDS_ACTIVE_STAGE);
-  const stageDateStart = format(new Date(stage.startDate), 'P');
-  const stageDateEnd = format(new Date(stage.endDate), 'P');
+  // const isFinished = lt(stage.id, TDS_ACTIVE_STAGE);
+  // const stageDateStart = format(new Date(stage.startDate), 'P');
+  // const stageDateEnd = format(new Date(stage.endDate), 'P');
 
   return (
     <li className={cn(className, isActive && activeClass)}>
       <div>
-        {stage.title} {isActive && '(LIVE!)'}
+        {/* {stage.title} {isActive && '(LIVE!)'}
         {isActive && <img src={iconRight} width={47} height={13} alt="" />}
         <br />
         {isFinished && <span>(finished {stageDateEnd})</span>}
-        {!isActive && !isFinished && <span>(coming {stageDateStart})</span>}
+        {!isActive && !isFinished && <span>(coming {stageDateStart})</span>} */}
+        {stage.title}
+        <br />
+        <span>(coming soon)</span>
       </div>
     </li>
   );
