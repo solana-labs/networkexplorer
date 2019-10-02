2ff62b05f6d552dcd5d25499f627f242e235ee41
Manuel Calavera <diserkin@gmail.com>
2019-10-02 10:15:37 -0700

    feat: disclaimer

--- src/v2/components/Footer/Partnership/index.jsx
+++ src/v2/components/Footer/Partnership/index.jsx
@@ -1,17 +1,19 @@
-import {Typography} from '@material-ui/core';
-import React from 'react';
+import React, {useState} from 'react';
 import Button from 'v2/components/UI/Button';
+import {Modal, Fade, Typography, Backdrop} from '@material-ui/core';
 import iconRight from 'v2/assets/icons/arrow-right.png';
 import Mixpanel from 'v2/mixpanel';
 
+import Disclaimer from './Disclaimer';
 import useStyles from './styles';
 
 const Partnership = () => {
+  const [isOpen, setOpen] = useState(false);
   const classes = useStyles();
-
   const handlePartner = () => Mixpanel.track('Clicked Partner with us');
-
   const handleDiscuss = () => Mixpanel.track('Clicked Discuss on Forums');
+  const openDisclaimer = () => setOpen(true);
+  const closeDisclaimer = () => setOpen(false);
 
   return (
     <div>
@@ -34,6 +36,7 @@ const Partnership = () => {
       </Button>
       <div className={classes.links}>
         <a
+          className={classes.link}
           href="https://forums.solana.com/"
           target="_blank"
           rel="noopener noreferrer"
@@ -42,11 +45,25 @@ const Partnership = () => {
           Discuss on forums
           <img src={iconRight} width={53} alt="" />
         </a>
-        <a href="https://solana.com/" target="_blank" rel="noopener noreferrer">
+        <button className={classes.link} onClick={openDisclaimer}>
           Website disclaimer
           <img src={iconRight} width={53} alt="" />
-        </a>
+        </button>
       </div>
+      <Modal
+        open={isOpen}
+        onClose={closeDisclaimer}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        className={classes.disclaimer}
+        BackdropProps={{
+          timeout: 300,
+        }}
+      >
+        <Fade in={isOpen}>
+          <Disclaimer onClose={closeDisclaimer} />
+        </Fade>
+      </Modal>
     </div>
   );
 };
