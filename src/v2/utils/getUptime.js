8e68ba02bca1d4342ed4ee394097cb99929588a8
Manuel Calavera <diserkin@gmail.com>
2019-10-02 21:37:14 -0700

    fix: uptime overflow

--- src/v2/utils/getUptime.js
+++ src/v2/utils/getUptime.js
@@ -1,7 +1,7 @@
 import {compose, getOr, multiply} from 'lodash/fp';
 
 export default compose(
-  time => parseFloat(time.toFixed(time ? 4 : 2)),
+  time => (time > 100 ? 100 : parseFloat(time.toFixed(time ? 4 : 2))),
   multiply(100),
   getOr(0, 'uptime.uptime.[0].percentage'),
 );
