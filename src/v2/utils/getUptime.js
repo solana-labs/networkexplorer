3528825cc79b86045c4a3699599f6bff52879fba
Sunny Gleason <sunny.gleason@gmail.com>
2019-10-03 09:32:35 -0400

    feat: uptime truncation (cosmetic, pending real fix)

--- src/v2/utils/getUptime.js
+++ src/v2/utils/getUptime.js
@@ -1,6 +1,12 @@
 import {compose, getOr, multiply} from 'lodash/fp';
 
 export default compose(
+  //
+  // FIXME: this masks possible calculation errors, see:
+  //
+  //    https://github.com/solana-labs/networkexplorer/issues/456
+  //    https://github.com/solana-labs/networkexplorer/issues/425
+  //
   time => (time > 100 ? 100 : parseFloat(time.toFixed(time ? 4 : 2))),
   multiply(100),
   getOr(0, 'uptime.uptime.[0].percentage'),
