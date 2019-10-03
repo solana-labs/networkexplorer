e84f4664513dd9fa52a9a437aaa8416799dbe2c9
Manuel Calavera <diserkin@gmail.com>
2019-10-02 21:16:23 -0700

    feat: action bars

--- src/AppV2.js
+++ src/AppV2.js
@@ -1,6 +1,7 @@
 import {CssBaseline, makeStyles} from '@material-ui/core';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import {observer} from 'mobx-react-lite';
+import {SnackbarProvider} from 'notistack';
 import React, {lazy, Suspense} from 'react';
 import {hot} from 'react-hot-loader/root';
 import {Route, Switch} from 'react-router-dom';
@@ -9,6 +10,7 @@ import Footer from 'v2/components/Footer';
 import theme from 'v2/theme';
 import FailedPanel from 'v2/components/UI/FailedPanel';
 import Socket from 'v2/stores/socket';
+import getColor from 'v2/utils/getColor';
 
 try {
   Socket.init();
@@ -33,41 +35,47 @@ const ApplicationDetail = lazy(() =>
 );
 const Favorites = lazy(() => import('v2/components/Favorites'));
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    overflow: 'hidden',
-    minHeight: '100vh',
-  },
-  content: {
-    marginLeft: 50,
-    minWidth: '1px',
-    padding: '50px 24px 0 24px',
-    maxWidth: 'calc(100% - 50px)',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: 0,
-      padding: 0,
-      paddingTop: 80,
-      maxWidth: '100%',
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      display: 'flex',
+      overflow: 'hidden',
+      minHeight: '100vh',
     },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-    [theme.breakpoints.down('md')]: {
-      minHeight: 85,
+    content: {
+      marginLeft: 50,
+      minWidth: '1px',
+      padding: '50px 24px 0 24px',
+      maxWidth: 'calc(100% - 50px)',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      [theme.breakpoints.down('sm')]: {
+        marginLeft: 0,
+        padding: 0,
+        paddingTop: 80,
+        maxWidth: '100%',
+      },
     },
-    [theme.breakpoints.up('md')]: {
-      minHeight: 50,
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+      [theme.breakpoints.down('md')]: {
+        minHeight: 85,
+      },
+      [theme.breakpoints.up('md')]: {
+        minHeight: 50,
+      },
     },
-  },
-}));
+    success: {
+      backgroundColor: getColor('grey2')(theme),
+      color: getColor('white')(theme),
+    },
+  };
+});
 
 const App = () => {
   const classes = useStyles();
@@ -75,46 +83,61 @@ const App = () => {
 
   return (
     <MuiThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <CssBaseline />
-        <Header />
+      <SnackbarProvider
+        classes={{
+          variantSuccess: classes.success,
+        }}
+        maxSnack={3}
+      >
+        <div className={classes.root}>
+          <CssBaseline />
+          <Header />
 
-        <div className={classes.content}>
-          <div className={classes.toolbar} />
-          {hasError && <FailedPanel />}
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route exact path="/validators" component={Validators} />
-              <Route exact path="/validators/all" component={ValidatorsAll} />
-              <Route exact path="/validators/:id" component={ValidatorDetail} />
-              <Route exact path="/tour-de-sol" component={TourDeSol} />
-              <Route exact path="/blocks" component={Blocks} />
-              <Route exact path="/blocks/timeline/:start" component={Blocks} />
-              <Route exact path="/blocks/:id" component={BlockDetail} />
-              <Route exact path="/transactions" component={Transactions} />
-              <Route
-                exact
-                path="/transactions/timeline/:start"
-                component={Transactions}
-              />
-              <Route
-                exact
-                path="/transactions/:id"
-                component={TransactionDetail}
-              />
-              <Route exact path="/applications" component={Applications} />
-              <Route
-                exact
-                path="/applications/:id"
-                component={ApplicationDetail}
-              />
-              <Route exact path="/favorites" component={Favorites} />
-            </Switch>
-          </Suspense>
-          <Footer />
+          <div className={classes.content}>
+            <div className={classes.toolbar} />
+            {hasError && <FailedPanel />}
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/validators" component={Validators} />
+                <Route exact path="/validators/all" component={ValidatorsAll} />
+                <Route
+                  exact
+                  path="/validators/:id"
+                  component={ValidatorDetail}
+                />
+                <Route exact path="/tour-de-sol" component={TourDeSol} />
+                <Route exact path="/blocks" component={Blocks} />
+                <Route
+                  exact
+                  path="/blocks/timeline/:start"
+                  component={Blocks}
+                />
+                <Route exact path="/blocks/:id" component={BlockDetail} />
+                <Route exact path="/transactions" component={Transactions} />
+                <Route
+                  exact
+                  path="/transactions/timeline/:start"
+                  component={Transactions}
+                />
+                <Route
+                  exact
+                  path="/transactions/:id"
+                  component={TransactionDetail}
+                />
+                <Route exact path="/applications" component={Applications} />
+                <Route
+                  exact
+                  path="/applications/:id"
+                  component={ApplicationDetail}
+                />
+                <Route exact path="/favorites" component={Favorites} />
+              </Switch>
+            </Suspense>
+            <Footer />
+          </div>
         </div>
-      </div>
+      </SnackbarProvider>
     </MuiThemeProvider>
   );
 };
