9c401e643e1fa5221cf1111ad79b0647ef7286c7
Manuel Calavera <diserkin@gmail.com>
2019-10-25 10:52:16 -0700

    fix: minor map tooltip refactoring

--- src/v2/components/UI/MapTooltip/index.jsx
+++ src/v2/components/UI/MapTooltip/index.jsx
@@ -3,27 +3,12 @@ import {Tooltip} from '@material-ui/core';
 import {TooltipProps} from '@material-ui/core/Tooltip';
 import React from 'react';
 
-import useStyles from './styles';
-
 type MapTooltipProps = {
   title: string,
 };
 
-const MapTooltip = ({title, ...rest}: MapTooltipProps & TooltipProps) => {
-  const {arrow, ...classes} = useStyles();
-
-  return (
-    <Tooltip
-      classes={{...classes, ...rest.classes}}
-      {...rest}
-      title={
-        <>
-          {title()}
-          <span className={arrow} />
-        </>
-      }
-    />
-  );
-};
+const MapTooltip = ({title, ...rest}: MapTooltipProps & TooltipProps) => (
+  <Tooltip classes={{...rest.classes}} {...rest} title={title()} />
+);
 
 export default MapTooltip;
