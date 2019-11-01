ac8d77205f2a380821de23eac72372ca2872d4d7
Manuel Calavera <diserkin@gmail.com>
2019-11-01 15:53:39 -0700

    fix: minor transactions details component and store refactoring

--- src/v2/components/Transactions/Detail/index.jsx
+++ src/v2/components/Transactions/Detail/index.jsx
@@ -1,8 +1,8 @@
 // @flow
 import {Container, Tabs, useTheme} from '@material-ui/core';
 import useMediaQuery from '@material-ui/core/useMediaQuery/useMediaQuery';
-import {map, eq} from 'lodash/fp';
-import React, {useState} from 'react';
+import {map} from 'lodash/fp';
+import React, {useEffect, useState} from 'react';
 import SectionHeader from 'v2/components/UI/SectionHeader';
 import QRPopup from 'v2/components/QRPopup';
 import InfoRow from 'v2/components/InfoRow';
@@ -10,7 +10,6 @@ import Loader from 'v2/components/UI/Loader';
 import TabNav from 'v2/components/UI/TabNav';
 import CopyBtn from 'v2/components/UI/CopyBtn';
 import {observer} from 'mobx-react-lite';
-import _ from 'lodash';
 import TransactionDetailStore from 'v2/stores/transactions/detail';
 import {Link, Match} from 'react-router-dom';
 import asTime from 'v2/utils/asTime';
@@ -25,9 +24,9 @@ const TransactionDetail = ({match}: {match: Match}) => {
   const classes = useStyles();
   const {
     isLoading,
-    transactionId,
     transaction,
     transactionView,
+    init,
   } = TransactionDetailStore;
 
   const [tab, setTab] = useState(0);
@@ -35,13 +34,19 @@ const TransactionDetail = ({match}: {match: Match}) => {
   const verticalTabs = useMediaQuery(theme.breakpoints.down('sm'));
   const handleTabChange = (event, tab) => setTab(tab);
 
-  if (transactionId !== match.params.id) {
-    TransactionDetailStore.init({transactionId: match.params.id});
-  }
+  useEffect(() => {
+    init({transactionId: match.params.id});
+  }, [init, match.params.id]);
 
   if (isLoading) {
-    return <Loader width="533" height="290" />;
+    return (
+      <Container className={classes.loader}>
+        <Loader width="533" height="290" />
+      </Container>
+    );
   }
+
+  if (!transaction) return null;
 
   const specs = [
     {
@@ -75,55 +80,41 @@ const TransactionDetail = ({match}: {match: Match}) => {
 
   const renderSpec = info => <InfoRow key={info.label} {...info} />;
 
-  const programMap = _.reduce(
-    transaction.instructions,
-    (a, i) => {
-      if (!a[i.program_id]) {
-        a[i.program_id] = i.program_id;
-      }
-
-      return a;
-    },
-    {},
-  );
-
-  const programs = _.values(programMap);
-
   const tabNav = [
-    `Programs: ${programs.length}`,
+    `Programs: ${transaction.instructions.length}`,
     'status: success',
     'code/source',
   ];
-
+  const tabs = {
+    0: <ProgramsTab transaction={transaction} />,
+    1: <ProgramStatus transaction={transaction} />,
+    2: <TransactionCode transactionView={transactionView} />,
+  };
   const renderTabNav = label => <TabNav key={label} label={label} />;
   const url = currentURL();
   return (
     <Container>
-      <div className={classes.root}>
-        <SectionHeader title="Transaction Detail">
-          <div className={classes.blockTitle}>
-            <span>{transaction.id}</span>
-            <CopyBtn text={transaction.id} />
-            <QRPopup url={url} />
-          </div>
-        </SectionHeader>
-        <div className={classes.body}>
-          <div className={classes.spec}>{map(renderSpec)(specs)}</div>
+      <SectionHeader title="Transaction Detail">
+        <div className={classes.blockTitle}>
+          <span>{transaction.id}</span>
+          <CopyBtn text={transaction.id} />
+          <QRPopup url={url} />
         </div>
-        <Tabs
-          orientation={verticalTabs ? 'vertical' : 'horizontal'}
-          className={classes.tabs}
-          classes={{indicator: classes.indicator}}
-          value={tab}
-          variant="fullWidth"
-          onChange={handleTabChange}
-        >
-          {map(renderTabNav)(tabNav)}
-        </Tabs>
-        {eq(0, tab) && <ProgramsTab transaction={transaction} />}
-        {eq(1, tab) && <ProgramStatus transaction={transaction} />}
-        {eq(2, tab) && <TransactionCode transactionView={transactionView} />}
+      </SectionHeader>
+      <div className={classes.body}>
+        <div className={classes.spec}>{map(renderSpec)(specs)}</div>
       </div>
+      <Tabs
+        orientation={verticalTabs ? 'vertical' : 'horizontal'}
+        className={classes.tabs}
+        classes={{indicator: classes.indicator}}
+        value={tab}
+        variant="fullWidth"
+        onChange={handleTabChange}
+      >
+        {map(renderTabNav)(tabNav)}
+      </Tabs>
+      {tabs[tab]}
     </Container>
   );
 };
